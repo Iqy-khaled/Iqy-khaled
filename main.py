c287f0190fed6d8757e47bc194b7d630af74eeb2
Iqy-khaled <131682457+Iqy-khaled@users.noreply.github.com>
2025-05-05 16:46:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 
-Names = [‘sparrow’, ‘lyran’, ‘fred’, ‘djasul’]
+Names = ['sparrow' , 'lyran' , 'fred' , 'djasul']
 
-print = {names [0] .title}
+print = (names [0] .title () )
 
-print = {names [1] .title}
+print = (names [1] .title () )
 
-print = {names [-1] .title}
+print = (names [-1] .title () )
 
-print = {names [-2] .title}
+print = (names [-2] .title () )
 
-greetings = f"Aidios AMAIGO {names [0] .title}”
+greetings = f"Aidios AMAIGO {names [0] .title () }”
 print = (greetings)
 
-greetings = f"Aidios AMAIGO {names [1] .title}”
+greetings = f"Aidios AMAIGO {names [1] .title () }”
 print = (greetings)
 
-greetings = f"Aidios AMAIGO {names [-1] .title}”
+greetings = f"Aidios AMAIGO {names [-1] .title () }”
 print = (greetings)
 
-greetings = f"Aidios AMAIGO {names [-2] .title}”
+greetings = f"Aidios AMAIGO {names [-2] .title () }”
 print = (greetings)

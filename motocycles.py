6b3d8441c9be1b286e328a3299d96617c4c0e0d8
Iqy-khaled <131682457+Iqy-khaled@users.noreply.github.com>
2025-05-12 16:46:09 +0300

    Update motocycles.py

--- motocycles.py
+++ motocycles.py
@@ -2,3 +2,18 @@ motorcycles = [ 'honda' , 'yamaha' , 'suzuki']
 
 motrcycles [0] = 'ducati'
 print(motorcycles)
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+friends = []
+
+friends.append('breeny')
+friends.append('jules')
+friends.append('djasul')
+
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+
+del friends[1]
+print(1)

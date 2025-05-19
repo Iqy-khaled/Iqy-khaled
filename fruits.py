bf00983e866b6cb9294cd70348e06fab73f7f281
Iqy-khaled <131682457+Iqy-khaled@users.noreply.github.com>
2025-05-19 16:48:01 +0300

    Create fruits.py

---
+++ fruits.py
@@ -0,0 +1,12 @@
+fruits = ['apple', 'banana', 'orange', 'pineapple']
+
+for fruits in fruits :
+    print(fruit)
+
+
+
+ballers = ['michael jordan', 'lamello ball', 'antony edwards', 'devin boker',]
+for ballers in ballers :
+    print(ballers)
+
+statement = [f "{ballers [0]. title} is the mvp of this season"]
